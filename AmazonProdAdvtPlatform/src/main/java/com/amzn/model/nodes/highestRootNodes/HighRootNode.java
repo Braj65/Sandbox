1852c02c76e922509fd0b67f61a6dc7dd1f13bea
sahob1 <brajminator@gmail.com>
2018-02-01 21:18:31 +0530

    Fixed all. Needs cleaning of duplicate codes

--- AmazonProdAdvtPlatform/src/main/java/com/amzn/model/nodes/highestRootNodes/HighRootNode.java
+++ AmazonProdAdvtPlatform/src/main/java/com/amzn/model/nodes/highestRootNodes/HighRootNode.java
@@ -13,53 +13,61 @@ import com.amzn.model.nodes.nodeEntity.ldapNodeEntity.AbstractParentNodeStats;
 import com.amzn.model.utility.LoadLdapChildren;
 import com.amzn.model.utility.loaderFactory.LoaderFactory;
 
-public class HighRootNode extends AbstractHighRootNode{
-    
-    
-    public HighRootNode(AbstractParentNodeStats rootNodeStats){
-	childNodes=new ArrayList<INode>();
-	ldapLoader=LoaderFactory.getPropLoader(rootNodeStats.getLdapFile());
-	currentRootCategory=rootNodeStats;
+public class HighRootNode extends AbstractHighRootNode {
+
+    public HighRootNode(AbstractParentNodeStats rootNodeStats) {
+	childNodes = new ArrayList<INode>();
+	ldapLoader = LoaderFactory.getPropLoader(rootNodeStats.getLdapFile());
+	currentRootCategory = rootNodeStats;
     }
 
-    public void loadChildren(){
+    public void loadChildren() {
 	ldapLoader.createChildObjectsFromChildFilex(childNodes, ldapLoader.getPropConfigObj());
     }
-    
-    @Override
-    public void interpretChild(){
-	PropertiesConfiguration tempLeafChildProp=null;
-	for(int i=0;i<1;i++){
-	    tempLeafChildProp=processFirstLdapObjectAndNestedChildren(childNodes.get(i));
+
+    public void interpretChild() {
+	for (int i = 0; i < childNodes.size(); i++) {
+	    childNodes.get(i).register(this);
+	    childNodes.get(i).loadChildren();
+	    childNodes.get(i).interpretChild();
+	    childNodes.set(i, null);	    
+	}
+    }
+
+    public void interpretChildx() {
+	PropertiesConfiguration tempLeafChildProp = null;
+	for (int i = 0; i < 1; i++) {
+	    tempLeafChildProp = processFirstLdapObjectAndNestedChildren(childNodes.get(i));
 	    childNodes.set(i, null);
 	}
-	for(int i=1;i<childNodes.size();i++){
-	    tempLeafChildProp=processRestLdapObjectAndNestedChildren(childNodes.get(i), tempLeafChildProp);
+	for (int i = 1; i < childNodes.size(); i++) {
+	    tempLeafChildProp = processRestLdapObjectAndNestedChildren(childNodes.get(i), tempLeafChildProp);
 	    childNodes.set(i, null);
 	}
     }
-    
-    public PropertiesConfiguration processFirstLdapObjectAndNestedChildren(INode child){
+
+    public PropertiesConfiguration processFirstLdapObjectAndNestedChildren(INode child) {
 	child.register(this);
 	child.loadChildren();
 	child.interpretChild();
-	return ((AbstractLdapChild) child).getLeafNodeProperties();	
+	return ((AbstractLdapChild) child).getLeafNodeProperties();
     }
-    
-    public PropertiesConfiguration processRestLdapObjectAndNestedChildren(INode child, 
-	    PropertiesConfiguration prevLdapChildLeafNodeProp){
+
+    public PropertiesConfiguration processRestLdapObjectAndNestedChildren(INode child,
+	    PropertiesConfiguration prevLdapChildLeafNodeProp) {
 	child.register(this);
 	((AbstractLdapChild) child).setLeafNodeProperties(prevLdapChildLeafNodeProp);
 	child.loadChildren();
 	child.interpretChild();
 	return ((AbstractLdapChild) child).getLeafNodeProperties();
     }
-    
-    public String toString(){
+
+    public String toString() {
 	return currentRootCategory.toString();
     }
+
     @Override
     public void notifyObserver(INodeStats nodeStats) {
-	observer.notifyObserver(nodeStats);	
+	observer.notifyObserver(nodeStats);
     }
 }
