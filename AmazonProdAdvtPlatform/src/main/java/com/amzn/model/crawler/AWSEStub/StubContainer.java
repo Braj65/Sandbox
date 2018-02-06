590634864496773a3b9a92a5fdd2ac09cc8c5b7d
sahob1 <brajminator@gmail.com>
2018-02-06 22:19:02 +0530

    adding stub simple steps

--- AmazonProdAdvtPlatform/src/main/java/com/amzn/model/crawler/AWSEStub/StubContainer.java
+++ AmazonProdAdvtPlatform/src/main/java/com/amzn/model/crawler/AWSEStub/StubContainer.java
@@ -1,5 +1,7 @@
 package com.amzn.model.crawler.AWSEStub;
 
+import org.apache.axiom.om.OMAbstractFactory;
+import org.apache.axiom.om.OMElement;
 import org.apache.axiom.om.OMFactory;
 import org.apache.axis2.AxisFault;
 import org.apache.axis2.transport.http.HTTPConstants;
@@ -8,20 +10,24 @@ import com.amazon.stubs.AWSECommerceServiceStub;
 
 public class StubContainer {
     protected static final StubContainer INSTANCE=new StubContainer();
+    private static final String ACCESSKEY="AWSAccessKeyId";
     private AWSECommerceServiceStub stub=null;
     
+    
+    
     private StubContainer(){
-	OMFactory omFactory=null;
 	try {
 	    stub=new AWSECommerceServiceStub();
 	    stub._getServiceClient().getOptions().setProperty(
 		    HTTPConstants.CHUNKED, "false");
-	    
-	    
+//	    getChild("AWSAccessKeyId")
 	} catch (AxisFault e) {
 	    // TODO Auto-generated catch block
 	    e.printStackTrace();
-	}
-	
+	}	
+    }
+    
+    private OMElement getChild(String localName, String nameSpaceURI, String prefix){
+	return OMAbstractFactory.getOMFactory().createOMElement(localName, nameSpaceURI, prefix);
     }
 }
