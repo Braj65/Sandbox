7a995b45b84add794aa836b80ffc3c4ff4a74fcd
sahob1 <brajminator@gmail.com>
2018-02-12 09:37:13 +0530

    loading requestparams class

--- AmazonProdAdvtPlatform/src/main/java/com/amzn/model/crawler/reqRespParams/RequestParams.java
+++ AmazonProdAdvtPlatform/src/main/java/com/amzn/model/crawler/reqRespParams/RequestParams.java
@@ -1,5 +1,7 @@
 package com.amzn.model.crawler.reqRespParams;
 
+import org.apache.axis2.databinding.types.PositiveInteger;
+
 import com.amazon.webservices.awsecommerceservice._2013_08_01.ItemSearch;
 import com.amazon.webservices.awsecommerceservice._2013_08_01.ItemSearchRequest;
 import com.amzn.model.nodesToBeCrawled.nodes.nodeEntity.NodeStats;
@@ -20,5 +22,16 @@ public class RequestParams {
 	itemSrchParamsReqArr=new ItemSearchRequest[cntOfAllowedReq];
 	//
 	itemSearchReq=new ItemSearch();
+    }
+    
+    public void loadItemSrchParams(){
+	itemSrchParamsReq.setSearchIndex(RequestParams.getSearchIndex());
+	itemSrchParamsReq.setBrowseNode(RequestParams.getBrowseNodeIs());
+	itemSrchParamsReq.setItemPage(new PositiveInteger(Integer.toString(pageNum)));
+	itemSrchParamsReq.setResponseGroup(ResponseParams.getRespParams());
+    }
+    
+    public void loadItemSrchArr(){
+	itemSrchParamsReqArr[pageNum%2]=itemSrchParamsReq;
     }
 }
