4579070899865a8646860b5c28bcc9cea63aadb8
sahob1 <brajminator@gmail.com>
2018-03-13 21:09:39 +0530

    still adding main flow

--- AmazonProdAdvtPlatform/src/main/java/com/amzn/model/crawler/commpacks/response/ResponseParams.java
+++ AmazonProdAdvtPlatform/src/main/java/com/amzn/model/crawler/commpacks/response/ResponseParams.java
@@ -2,16 +2,26 @@ package com.amzn.model.crawler.commpacks.response;
 
 import com.amazon.webservices.awsecommerceservice._2013_08_01.ItemAttributes_type0;
 import com.amazon.webservices.awsecommerceservice._2013_08_01.Item_type3;
+import com.amazon.webservices.awsecommerceservice._2013_08_01.OfferSummary_type0;
 import com.amazon.webservices.awsecommerceservice._2013_08_01.Price;
+import com.amazon.webservices.awsecommerceservice._2013_08_01.VariationSummary_type0;
 
 public class ResponseParams {
     
     public String ASIN;
-    public Price price;
+    public OfferSummary_type0 offer;
+    public VariationSummary_type0 variations;
+    public Price OLowNewPrice, VLowSalePrice, VLowPrice;
     public ItemAttributes_type0 itemAttribs;
     
     public void populate(Item_type3 currItem) {
-	
+	ASIN=currItem.getASIN();
+	itemAttribs=currItem.getItemAttributes();
+	offer=currItem.getOfferSummary();
+	variations=currItem.getVariationSummary();
+	OLowNewPrice=offer.getLowestNewPrice();
+	VLowSalePrice=variations.getLowestSalePrice();
+	VLowPrice=variations.getLowestPrice();
     }
 
 }
