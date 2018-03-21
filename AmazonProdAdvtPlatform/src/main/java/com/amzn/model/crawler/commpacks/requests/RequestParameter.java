71f4ae34757e8f5510358ffcb43b6868f3024394
sahob1 <brajminator@gmail.com>
2018-03-21 21:22:08 +0530

    encapsulating changes

---
+++ AmazonProdAdvtPlatform/src/main/java/com/amzn/model/crawler/commpacks/requests/RequestParameter.java
@@ -0,0 +1,17 @@
+package com.amzn.model.crawler.commpacks.requests;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.axis2.databinding.types.PositiveInteger;
+
+public class RequestParameter {
+    
+    public Map<String, String> strReqParams=new HashMap<String, String>();
+    public Map<String, PositiveInteger> posIntReqParams=new HashMap<String, PositiveInteger>();
+    public String[] responseGroup;
+    
+    public RequestParameter(String[] respGrp){
+	responseGroup=respGrp;
+    }
+}
