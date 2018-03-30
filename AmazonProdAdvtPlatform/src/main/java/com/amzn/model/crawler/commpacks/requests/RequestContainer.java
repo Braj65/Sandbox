6cb6c2eabe04ae980d3c457176cd905861f142fd
sahob1 <brajminator@gmail.com>
2018-03-30 17:32:08 +0530

    a clean up done. Next add functionality for browsenodelookup

--- AmazonProdAdvtPlatform/src/main/java/com/amzn/model/crawler/commpacks/requests/RequestContainer.java
+++ AmazonProdAdvtPlatform/src/main/java/com/amzn/model/crawler/commpacks/requests/RequestContainer.java
@@ -1,23 +1,16 @@
 package com.amzn.model.crawler.commpacks.requests;
 
-import java.rmi.RemoteException;
-
-import org.apache.axis2.databinding.ADBBean;
 import org.apache.axis2.databinding.types.PositiveInteger;
 
 import com.amazon.webservices.awsecommerceservice._2013_08_01.ItemSearch;
 import com.amazon.webservices.awsecommerceservice._2013_08_01.ItemSearchRequest;
-import com.amzn.model.crawler.commpacks.response.ResponseHolder;
 import com.amzn.model.crawler.stub.AWESProperty;
-import com.amzn.model.crawler.stub.StubFactory;
 import com.amzn.model.nodesToBeCrawled.nodes.nodeEntity.INodeStats;
-import com.amzn.model.nodesToBeCrawled.nodes.nodeEntity.NodeStats;
 
 public class RequestContainer {
     
     private final Integer ITEM_PAGE_START_FOR_PRECREATE=1, ITEM_PAGE_END_FOR_PRECREATE=10;
     
-    public RequestParameter reqParameters;
     private INodeStats incomingNode;
     public String[] responseGroup;
     //unwise to save sort params in RequestPool and share between polled requestcontainers
@@ -30,10 +23,9 @@ public class RequestContainer {
     private ItemSearch srchReqContainer;
 
     public RequestContainer(){
-	reqParameters=new RequestParameter();
 	srchReqContainer=new ItemSearch();
     }
-    //STARTS
+    
     public ItemSearch createAmznSrchReq(){
 	if(srchRequest==null){
 	    srchRequest=new ItemSearchRequest();
@@ -48,42 +40,6 @@ public class RequestContainer {
 	return srchReqContainer;
     }
     
-    public RequestContainer setSortParamInSrhcReq(String sortParam){
-	srchRequest.setSort(sortParam);
-	return this;
-    }
-    
-    public void setSortParam(String[] sortParams){
-	this.sortParams=sortParams;
-    }
-    
-    public ResponseHolder dryRequestToFetchMeta(){
-	srchRequest.setSearchIndex(incomingNode.getSrchIndex());
-	srchRequest.setBrowseNode(incomingNode.getNodeId().toString());
-	srchRequest.setItemPage(new PositiveInteger(Integer.toString(1)));
-	srchRequest.setResponseGroup(responseGroup);
-	/*ItemSearchRequest[] srchReqArr={srchRequest};*/
-	ItemSearch itemSrch=new ItemSearch();
-	itemSrch.addRequest(srchRequest);
-	itemSrch.setAssociateTag(AWESProperty.Value.ASSOCIATE_TAG.getString());
-	
-	try {
-	    return StubFactory.getStubInstance("ItemSearch").executeOperation(itemSrch);
-	} catch (RemoteException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	}
-	return ResponseHolder.NULLINSTANCE;
-    }
-    
-    public void setRespGrp(String[] respGrp){
-	responseGroup=respGrp;
-    }
-    
-    public void setNodeStats(INodeStats stats){
-	incomingNode=stats;
-    }
-    //ENDS
     public void createItemSrchRepo(){
 	int currentPage=ITEM_PAGE_START_FOR_PRECREATE;
 	while(currentPage<=ITEM_PAGE_END_FOR_PRECREATE){
@@ -102,18 +58,21 @@ public class RequestContainer {
     
     public RequestContainer loadSrchIndexNodeid(INodeStats stats){
 	incomingNode=stats;
-//	addSrchIndexAndNodeIdToSrchReqs();
-	reqParameters.setNodeStats(stats);
 	return this;
     }
     
     public RequestContainer loadResponseGroup(String[] respGrp){
 	this.responseGroup=respGrp;
-	/*for(ItemSearchRequest srchReq:srchReqArr){
-	    srchReq.setResponseGroup(respGrp);
-	}*/
-	reqParameters.setRespGrp(respGrp);
 	return this;
+    }
+    
+    public RequestContainer setSortParamInSrhcReq(String sortParam){
+	srchRequest.setSort(sortParam);
+	return this;
+    }
+    
+    public void setSortParam(String[] sortParams){
+	this.sortParams=sortParams;
     }
     
     public void load(){
